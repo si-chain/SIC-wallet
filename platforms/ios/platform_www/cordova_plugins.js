9b310ab0a6f441fe8337fb469b81ed19e64514f5
jinhaidi1 <1753612833@qq.com>
2018-05-02 14:22:02 +0800

    add ios platforms

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-android-permissions.Permissions",
+    "file": "plugins/cordova-plugin-android-permissions/www/permissions-dummy.js",
+    "pluginId": "cordova-plugin-android-permissions",
+    "clobbers": [
+      "cordova.plugins.permissions"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-android-permissions": "1.0.0"
+};
+// BOTTOM OF METADATA
+});
