74d65497ef90872d49fa3458363c83a3c698b21a
jinhaidi <haidi.jin@baodanyun-inc.com>
2018-05-29 14:55:51 +0800

    dev config

--- walletApp/src/libs/env.js
+++ walletApp/src/libs/env.js
@@ -1,6 +1,7 @@
 /*eslint-disable */
 var env = {
-  httpEndpoint: 'http://XXXX.com.cn:XXXX', //dawn-3.0.0 跳过易签名
+  // httpEndpoint: 'http://XXXX.com.cn:XXXX', //dawn-3.0.0 跳过易签名
+  httpEndpoint: 'http://n0.sichain.com.cn:8888', //dawn-3.0.0 跳过易签名
   //mockTransactions: () => 'pass',
   expireInSeconds: 60,
   broadcast: true,
