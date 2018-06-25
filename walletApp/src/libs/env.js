e41e7f801406cc970eba5ddb8b4b36b1f9bcb9f7
jinhaidi <haidi.jin@baodanyun-inc.com>
2018-06-25 11:33:24 +0800

    add env

--- walletApp/src/libs/env.js
+++ walletApp/src/libs/env.js
@@ -1,9 +1,7 @@
 /*eslint-disable */
 var env = {
-  // httpEndpoint: 'http://XXXX.com.cn:XXXX', //@14.1.1 跳过易签名
-  chainId: '3941e117717490abdda3025b89fc4c03342e6c42ca5234e5b095db652b802523',
-  httpEndpoint: 'http://n0.sichain.com.cn:8888', //@14.1.1 跳过易签名
-  // httpEndpoint: 'http://59.110.229.74:8888',
+  httpEndpoint: 'http://XXXX.com.cn:XXXX', //@14.1.1 跳过易签名
+  chainId: 'XXXXXX',
   //mockTransactions: () => 'pass',
   expireInSeconds: 60,
   broadcast: true,
